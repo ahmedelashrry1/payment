f6ad1f3450cb81f3c08ff03d7a37f40d3234e6d6
ahmedelashrry1 <ahmedelashry888@gmail.com>
2024-11-28 01:19:24 +0200

    creat app bar

--- lib/presntation/view/my_cart_view.dart
+++ lib/presntation/view/my_cart_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:payment/style.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios_new_outlined),
+        centerTitle: true,
+        title:const Text(
+          'My Cart',
+          textAlign: TextAlign.center,
+          style: StyleS.style25,
+        ),
+      ),
+    );
   }
 }
+
